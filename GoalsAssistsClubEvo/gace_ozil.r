2ba73853a1a8ce6f76639219f56926453296aa34
DevBen8 <pimpaudben@gmail.com>
2017-08-22 12:16:41 +0200

    add comment

--- GoalsAssistsClubEvo/gace_ozil.r
+++ GoalsAssistsClubEvo/gace_ozil.r
@@ -3,7 +3,13 @@ library(tidyverse)
 library(hrbrthemes)
 library(reshape2)
 
+# exemple url : https://www.transfermarkt.com/mesut-ozil/leistungsdatenverein/spieler/35664
+
+# data
 ozil = data.frame(club=c("Schalke 04","Werder Bremen","Real Madrid","Arsenal"),Goals=c(1*90/2268,16*90/8279,27*90/11377,32*90/13810),Assists=c(5*90/2268,54*90/8279,81*90/11377,57*90/13810))
+
+# reshape for ggplot barchart
 ozil.dodge = melt(ozil)
 
+# plot
 ggplot(data=ozil.dodge,aes(x=club)) + geom_bar(aes(y=value,fill=variable),stat="identity",position=position_dodge()) + scale_fill_manual(values=c("#1de9b6", "#40c4ff")) + scale_x_discrete(limits=c("Schalke 04","Werder Bremen","Real Madrid","Arsenal")) +labs(x="", y="per 90 minutes",title="Mesut Ozil goals and assists evolution",subtitle="",caption="by Benoit Pimpaud / @Ben8t",color="") + theme_ipsum_rc()

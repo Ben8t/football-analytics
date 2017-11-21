5c9cc55d16d66454eb7ad4b71df47455d0f4aab4
DevBen8 <pimpaudben@gmail.com>
2017-11-21 20:42:12 +0100

    finish separate model/data_processing

--- DL-FootballForecast/data_processing.py
+++ DL-FootballForecast/data_processing.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import calendar
 
 def data_processing(data):
--- DL-FootballForecast/model.py
+++ DL-FootballForecast/model.py
@@ -1,7 +1,10 @@
 # model.py
 
 from data_processing import *
+import numpy as np
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense,Dropout
 
 # Load datasets
 E1415 = pd.read_csv('data/E1415.csv')
@@ -18,3 +21,29 @@ processed_data=data_processing(data)
 
 # Modelisation
 data_model = processed_data.head(processed_data.shape[0]-E1718.shape[0]) # remove 2017/2018 data, used later for testing
+
+x_train,y_train,x_test,y_test = split_dataset(data_model,0.3,['FTR_A','FTR_D','FTR_H'])
+
+# Keras neural network
+model = Sequential()
+model.add(Dense(200, input_dim=x_train.shape[1], activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(250, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(3,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_train.values,y_train.values, epochs=20, batch_size=10)

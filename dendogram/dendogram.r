48ee93273de5e9e870570aa00e41208d722f0935
Benoit Pimpaud <pimpaudben@gmail.com>
2018-01-12 22:11:51 +0100

    add dendo

--- dendogram/dendogram.r
+++ dendogram/dendogram.r
@@ -11,18 +11,17 @@ library(hrbrthemes)
 data <- read.csv2("data.csv") 
 data <- data  %>% filter(Minutes.played>=3000, Goals.per.90>=0.3) %>% column_to_rownames('Player') %>% select(-Position)
 
-data = data %>% select(Dribbles.per.90,Goals.per.90)
 
 dd <- dist(scale(data), method = "euclidean")
 hc <- hclust(dd, method = "ward.D2")
 
-dend <- fviz_dend(hc, k = 4, # Cut in four groups
-          cex = 0.5, # label size
-          #k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+dend <- fviz_dend(hc, k = 8, # Cut in four groups
+          cex = 0.6, # label size
+          # k_colors = c("#f06292", "#40c4ff", "#ffd740", "#1de9b6"),
           color_labels_by_k = TRUE, # color labels by groups
-          #rect = TRUE, # Add rectangle around groups
-          #rect_border = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"), 
-          #rect_fill = TRUE,
+          # rect = TRUE, # Add rectangle around groups
+          # rect_border = c("#f06292", "#40c4ff", "#ffd740", "#1de9b6"), 
+          # rect_fill = TRUE,
           horiz = TRUE)
 
-dend + theme_ipsum_rc() + labs(x="", y="Distance",title="Cluster Dendogram",subtitle="Hierarchical clustering with Dribbles and Goals per 90 minutes",caption='by Benoit Pimpaud / @Ben8t',color="") + theme(axis.ticks=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())
+dend + theme_ipsum_rc() + labs(x="", y="Distance",title="Cluster Dendogram",subtitle="Hierarchical clustering with all statistics",caption='by Benoit Pimpaud / @Ben8t',color="") + theme(axis.ticks=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())

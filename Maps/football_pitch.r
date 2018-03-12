7864cd83d7b0cd057d17d35a69462aeb81937e14
Benoit Pimpaud <pimpaudben@gmail.com>
2018-03-12 16:59:41 +0100

    add football_pitch and a toolbox for map (function definition)

---
+++ Maps/football_pitch.r
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(ggforce)
+
+background_color = "#8DE3C1"
+foreground_color = "#FDFEFE"
+
+ggplot() +  geom_rect(aes(xmin = 0, xmax = 105, ymin = 0, ymax = 68), #entire pitch with FIFA dimensions
+            fill = background_color, 
+            colour = foreground_color, 
+            size = .5) +
+            geom_circle(aes(x0 = 105 / 2, y0 = 68 / 2, r = 9.15), colour=foreground_color) + #centre circle
+            geom_circle(aes(x0 = 11, y0 = 68 / 2, r = 9.15), colour=foreground_color) + #penalty arc
+            geom_circle(aes(x0 = 105 - 11, y0 = 68 / 2, r = 9.15), colour=foreground_color) + #penalty arc
+            geom_rect(aes(xmin = 0, xmax = 16.5, ymin = 68 / 2 - 7.32 / 2 - 16.5, ymax = 68 / 2 + 7.32 / 2 + 16.5), #penalty box
+                      fill = background_color, 
+                      colour = foreground_color, 
+                      size = .5) +
+            geom_rect(aes(xmin = 105 - 16.5, xmax = 105, ymin = 68 / 2 - 7.32 / 2 - 16.5, ymax = 68 / 2 + 7.32 / 2 + 16.5),  #penalty box
+                      fill = background_color, 
+                      colour = foreground_color, 
+                      size = .5) +
+            geom_point(aes(x = 11, y = 68 / 2), colour=foreground_color) + #penalty spot
+            geom_point(aes(x = 105 -11, y = 68 / 2), colour=foreground_color) + #penalty spot
+            geom_segment(aes(x = -.5, xend = -.5, y = 68 / 2 + 7.32 / 2, yend = 68 / 2 - 7.32 / 2), colour=foreground_color) + #goal
+            geom_segment(aes(x = 105.5, xend = 105.5, y = 68 / 2 + 7.32 / 2, yend = 68 / 2 - 7.32 / 2), colour=foreground_color) + #goal
+            geom_rect(aes(xmin = 0, xmax = 5.5, ymin = 68 / 2 - 7.32 / 2 - 5.5, ymax = 68 / 2 + 7.32 / 2 + 5.5), #6 yard box
+                      fill = background_color, 
+                      colour = foreground_color, 
+                      size = .5) +
+            geom_rect(aes(xmin = 105 - 5.5, xmax = 105, ymin = 68 / 2 - 7.32 / 2 - 5.5, ymax = 68 / 2 + 7.32 / 2 + 5.5),  #6 yard box
+                      fill = background_color, 
+                      colour = foreground_color, 
+                      size = .5) +
+            geom_segment(aes(x = 105 /2, y = 0, xend = 105 / 2, yend = 68), colour=foreground_color) + #halfway line
+            coord_fixed() +
+              theme(rect = element_blank(), #remove additional ggplot2 features: lines, axis, etc...
+                  line = element_blank(),axis.title.y = element_blank(),
+                  legend.position = "none", 
+                  axis.title.x = element_blank(),
+                  axis.text.x = element_blank(),
+                  axis.text.y = element_blank())
---
+++ Maps/toolbox_map.r
@@ -0,0 +1,118 @@
+# toolbox_map.r
+# Function definition for football map such as passnetwork, shot locations, etc...
+
+library(tidyverse)
+library(hrbrthemes)
+library(ggforce)
+library(jsonlite)
+library(grid)
+library(gridExtra)
+library(igraph)
+library(ggnetwork)
+
+data = jsonlite::fromJSON(jsonfile)
+
+
+# EVENT PROCESSING #
+event_to_dataframe <- function(data){
+    event = data.frame(id=data$events$id,
+                       eventId=data$events$eventId,
+                       minute=data$events$minute,
+                       second=data$events$second,
+                       teamId=data$events$teamId,
+                       playerId=data$events$playerId,
+                       x=data$events$x,
+                       y=data$events$y,
+                       typeValue=data$events$type$value,
+                       typeDisplayName=data$events$type$displayName,
+                       period=data$events$period$value,
+                       isTouch=data$events$isTouch,
+                       outcome=data$events$outcomeType$value)
+    return(event)
+}
+
+playersId_playersNames <- function(data){
+    playersId_playersNames = data$playerIdNameDictionary %>% 
+        t %>% 
+        as.data.frame() %>% 
+        t %>% 
+        as.data.frame() %>% 
+        tibble::rownames_to_column() %>% 
+        select(playerName=V1,playerId=rowname) %>% 
+        mutate(playerId=as.numeric(playerId),playerName=unlist(playerName))
+    return(playersId_playersNames)
+}
+
+teamsId_teamsNames <- function(data){
+    teamsId_teamsNames = data.frame(teamId=c(data$home$teamId, data$away$teamId),
+                                    teamName=c(data$home$name, data$away$name))
+    return(teamsId_teamsNames)
+}
+
+event_cleaning <- function(data){
+    # return all event with eventId, minute, second, teamName, playerName, x, y, typeDisplayName, perido, isTouch, outcome
+    # clearing event data 
+    event_cleared = event_to_dataframe(data) %>%
+            select(eventId, minute, second, teamId, playerId, x, y, typeDisplayName, period, isTouch, outcome) %>%
+            mutate(x=105*x/100, y=68*y/100) %>% # normalize x and y to fit with FIFA dimensions
+            na.omit()
+
+    # get player and teams name with corresponding id
+    playersId_playersNames = playersId_playersNames(data)
+    teamsId_teamsNames = teamsId_teamsNames(data)
+
+    # join all
+    simple_event = left_join(event_cleared, playersId_playersNames, by=c("playerId")) %>%
+            left_join(., teamsId_teamsNames, by=c("teamId")) %>%
+            select(-teamId, -playerId)
+
+    return(simple_event)
+}
+
+lineup_from_event <- function(data){
+    lineup = list()
+    lineup$home_lineup_id = data$home$formations$playerIds[[1]] %>% as.data.frame() %>% select(.,playerId=.) %>% slice(1:11) %>% as.data.frame()
+    lineup$away_lineup_id = data$home$formations$playerIds[[1]] %>% as.data.frame() %>% select(.,playerId=.) %>% slice(1:11) %>% as.data.frame()
+    return(lineup)
+}
+
+load_many_games <- function(files){
+    # file_list = list.files("data/arsenal1718/", full.names=TRUE)
+    # dataframe_events = load_many_games(file_list)
+    dataframe_events <- list()
+    i=1
+    for(file in files){
+        print(file)
+        data = jsonlite::fromJSON(file)
+        dataframe_events[[i]] <- data
+        i = i + 1
+    }
+    return(dataframe_events)
+}
+
+# MAPS PROCESSING #
+get_all_from_game_player <- function(data, player){
+    event = event_cleaning(data) %>%
+            filter(playerName==player) %>%
+            select(playerName, x, y, typeDisplayName, minute, period)
+    return(event)
+}
+
+get_all_from_game_team <- function(data, team){
+    event = event_cleaning(data) %>%
+            filter(teamName==team) %>%
+            select(teamName, playerName, x, y, typeDisplayName, minute, period)
+    return(event)
+}
+
+gather_all_from_many_games_player <- function(dataframe_events, player){
+    # dataframe_events is a list of each games event dataframe
+    final_data = data.frame()
+    for(data in dataframe_events){
+        game_player = get_all_from_game_player(data, player)
+        final_data = rbind(final_data, game_player)
+    }
+    return(final_data)
+}
+
+

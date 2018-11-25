b2495869774085e7b4ab9ad49ddaae4d9d64d377
Benoit Pimpaud <pimpaudben@gmail.com>
2018-11-25 15:42:12 +0100

    add rollmean trend xg xgc

---
+++ visualisation/rollmean/main.r
@@ -0,0 +1,45 @@
+library(readr)
+library(zoo)
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
+library(hrbrthemes)
+
+
+raw_data <- read_csv("xg-ars.csv")
+id = 13
+subtitle = "Arsenal - 2015 to 2017 Premier League season"
+
+data <- raw_data %>% 
+    mutate(team=ifelse(team_id==id, "xG", "xGC"), id=row_number()) %>% 
+    select(startDate, team, xG)
+
+grouped_data <- data %>% 
+    group_by(startDate, team) %>% 
+    summarise(xg=sum(xG)) %>% as.data.frame() %>%
+    mutate(date=format(as.Date(startDate), format="%Y-%m-%d"))
+grouped_data$id <- rep(1:(nrow(grouped_data)/2), each=2)
+
+ggplot() + 
+    geom_line(data=grouped_data %>% filter(team=="xG"), aes(x=id, y=rollmean(xg, 15, na.pad=TRUE), color=team)) +
+    geom_point(data=grouped_data %>% filter(team=="xG"), aes(x=id, y=rollmean(xg, 15, na.pad=TRUE), , color=team)) +
+    scale_color_manual(values=c("#56FFAE", "#C64E96")) +
+    geom_line(data=grouped_data %>% filter(team=="xGC"), aes(x=id, y=rollmean(xg, 15, na.pad=TRUE), color=team)) +
+    geom_point(data=grouped_data %>% filter(team=="xGC"), aes(x=id, y=rollmean(xg, 15, na.pad=TRUE), color=team)) + 
+    geom_vline(xintercept=39, color="white", linetype="dashed") +
+    geom_text(aes(x=39, y=0.3, label="\nchange in season"), colour="white", angle=90, size=3)+
+    geom_vline(xintercept=77, color="white", linetype="dashed") +
+    geom_text(aes(x=77, y=0.3, label="\nchange in season"), colour="white", angle=90, size=3)+
+    scale_x_continuous(breaks=seq(1,nrow(grouped_data)/2-5, 5), limits=c(15,nrow(grouped_data)/2-5), labels=unique(grouped_data$date)[seq(1,nrow(grouped_data)/2-5, 5)]) +
+    scale_y_continuous(breaks=c(0,0.5,1,1.5,2,2.5,3), limits=c(0,3)) +
+    labs(x="", y="xG/xGC",title="xG/xGC rolling averages",subtitle=subtitle,caption="15 games rolling averages\nby @Ben8t",color="") +
+    theme_ipsum_rc() +
+    theme(
+        plot.title = element_text(size=35),
+        text = element_text(colour="white"),
+        axis.text.y = element_text(colour="white"),
+        axis.text.x = element_text(colour="white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_rect(fill = "#2162AA")) +
+    theme(axis.text.x = element_text(angle = 30, hjust = 1))

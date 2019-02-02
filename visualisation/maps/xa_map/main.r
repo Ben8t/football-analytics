b5f5570a56a358373d626887f270c27b5efa6ccb
Benoit Pimpaud <pimpaudben@gmail.com>
2019-02-02 16:53:02 +0100

    clean folder

---
+++ visualisation/maps/xa_map/main.r
@@ -0,0 +1,40 @@
+# xA map
+
+library(dplyr)
+library(rvest)
+library(hrbrthemes)
+library(magick)
+library(viridis)
+library(ggforce)
+library(gtable)
+library(gridExtra)
+library(grid)
+library(readr)
+
+source(file="src/xa_map.r")
+
+# Launcher 
+data_file <- "data/wolves_conceded_xa.csv"
+text <- "Wolverhampton conceded passes from 2018-2019 Premier League season"
+final_filename <- "wolve_xa_map_1819.png"
+
+# Colors
+background_color <- "#2162AA"
+foreground_color <- "#F7F6F4"
+text_color <- "white"
+color1 <- "#64BEF3"
+color2 <- "#FFCB41"
+color3 <- "#56FFAE"
+high_gradient_color <- "#64BEF3"
+
+# Load data
+data <-  read_csv(data_file)
+
+# Filter data
+# you can filter on team_name or player_name
+# data <- data %>% filter(player_name=="Olivier Giroud")
+
+# Build map
+stats <- get_stats(data)
+map <- xa_map(data, background_color, foreground_color, color1, color2, color3, high_gradient_color)
+create_graphic(map, text, stats, final_filename, background_color, text_c)
--- visualisation/maps/xa_map/src/xa_map.r
+++ visualisation/maps/xa_map/src/xa_map.r
@@ -1,27 +1,12 @@
-# xA map
-
-library(dplyr)
-library(rvest)
-library(hrbrthemes)
-library(magick)
-library(viridis)
-library(ggforce)
-library(gtable)
-library(gridExtra)
-library(grid)
-library(readr)
-
-background_color = "#2162AA"
-foreground_color = "#F7F6F4"
-
-xa_map <- function(data, background_color, foreground_color, c1, c2, c3){
+# xa_map.r
+xa_map <- function(data, background_color, foreground_color, color1, color2, color3, high_gradient_color){
       data <- data %>% filter(xA >= 0 | is_assist == 1)
       data$xA_cut = cut(data$xA, breaks=c(0,0.1,1), right = FALSE)
-      data = data %>% mutate(xA_cut = ifelse(is_assist==1, "assist", xA_cut))
+      dataa = data %>% mutate(xA_cut = ifelse(is_assist==1, "assist", xA_cut))
       if(length(table(data$xA_cut)) == 3){
-            colors_value = c(c1, c2, c3)
+            colors_value = c(color1, color2, color3)
       }else{
-            colors_value = c(c1, c3)
+            colors_value = c(color1, color3)
       }
       print(colors_value)
       ggplot() +  geom_rect(aes(xmin = 0, xmax = 68, ymin = 0, ymax = 105), #entire pitch with FIFA dimensions
@@ -58,7 +43,7 @@ xa_map <- function(data, background_color, foreground_color, c1, c2, c3){
             axis.text.x = element_blank(),
             axis.text.y = element_blank()) +
       stat_density2d(data=data, aes(x=-y_end+68,y=x_end,fill=..level..), alpha=0.3,geom="polygon", show.legend=FALSE) +
-      scale_fill_gradient(low = background_color, high = "#64BEF3") + 
+      scale_fill_gradient(low = background_color, high = high_gradient_color) + 
       geom_point(data=data, aes(x=-y_begin + 68, y=x_begin, color=xA_cut, size=xA)) + 
       scale_color_manual(values=colors_value) + 
       coord_cartesian(ylim=c(55, 105))
@@ -71,33 +56,15 @@ get_stats <- function(data){
     paste0("Total xA = ", total_xa, "\nAssists = ", assist, "\nPasses = ", passes)
 }
 
-create_graphic <- function(xa_map, text, stats, filepath){
-    ggsave(filename="visualisation/maps/img/g_xamap_tmp.png", xa_map + theme(plot.margin=unit(c(3.5,0,-0.3,0),"cm")), width=10.5, height=8, dpi=150, bg="#2162AA")
-    xg_map <- image_read("visualisation/maps/img/g_xamap_tmp.png")
-    title <- image_read("visualisation/maps/template/xa_map/title.png")
-    foreground <- image_read("visualisation/maps/template/xa_map/foreground.png")
+create_graphic <- function(xa_map, text, stats, filepath, background_color, text_color){
+    ggsave(filename="img/g_xamap_tmp.png", xa_map + theme(plot.margin=unit(c(3.5,0,-0.3,0),"cm")), width=10.5, height=8, dpi=150, bg=background_color)
+    xg_map <- image_read("img/g_xamap_tmp.png")
+    title <- image_read("template/title.png")
+    foreground <- image_read("template/foreground.png")
     full_image <- xg_map %>%
         image_composite(image_scale(title,"600"), offset="+70-40") %>%
         image_composite(foreground) %>%
-        image_annotate(text, font="Roboto", size=35, location="+80+190", color="white") %>%
-        image_annotate(stats, font="Roboto", size=25, location="+120+725", color="white")
+        image_annotate(text, font="Roboto", size=35, location="+80+190", color=text_color) %>%
+        image_annotate(stats, font="Roboto", size=25, location="+120+725", color=text_color)
     image_write(full_image, path=filepath)
 }
-
-# Launcher 
-data_file = "wolves_conceded_xa.csv"
-text = "Wolverhampton conceded passes from 2018-2019 Premier League seasons"
-final_filename = "tmp_xamap.png"
-
-# load data
-data <-  read_csv(data_file)
-# filter data
-# you can filter on team_name or player_name
-# data <- data %>% filter(player_name=="Olivier Giroud")
-
-stats <- get_stats(data)
-# build xG map
-map <- xa_map(data, "#2162AA", "#F7F6F4","#64BEF3", "#FFCB41", "#56FFAE")
-
-# build full graphic
-create_graphic(map, text, stats, final_filename)

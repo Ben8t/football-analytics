256c1bd62a69b5d0a24ae419f91b7c74606eab52
Benoit Pimpaud <pimpaudben@gmail.com>
2019-02-10 13:58:22 +0100

    update request

--- src/sql_queries/touch_5_minutes_bin.sql
+++ src/sql_queries/touch_5_minutes_bin.sql
@@ -1,6 +1,6 @@
 SELECT
     *,
-    5 * (TRUNC(time/300) + 1) AS bin
+    10 * (TRUNC(time/600) + 1) AS bin
 FROM (
     SELECT
         minute * 60 + second AS time,
@@ -10,11 +10,6 @@ FROM (
         y,
         team_id
     FROM events
-    WHERE game_id = '090a665e80376ff612a4201e81590f84' AND is_touch = 1
+    WHERE game_id = 'bc5f0c1e29733ac5bc94c6e84cdb641f' AND is_touch = 1
 ) AS base
 ORDER BY time
-
-ggplot(data=data %>% filter(team_id==175), aes(x=x, y=y)) + geom_point() + labs(title = 'Minute bin: {frame_time}') + transition_time(bin) +
-+     ease_aes('linear')
-> animate(a, 
-+         duration = 20)

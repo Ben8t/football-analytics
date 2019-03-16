7f57efee551ac37f23e029712df817e3230a286d
Benoit Pimpaud <pimpaudben@gmail.com>
2019-03-16 15:03:01 +0100

    add image processing

--- model/pass2vec/src/SequenceFactory.py
+++ model/pass2vec/src/SequenceFactory.py
@@ -2,22 +2,25 @@ from model.pass2vec.src.Pass import Pass
 from itertools import islice, tee
 import pandas
 import numpy
-
-
+import cv2
+import datetime
 class SequencesFactory:
 
     @staticmethod
-    def build_pass_list(data):
+    def to_bin(value, step):
+        return numpy.floor(value / step) * step
+
+    def build_pass_list(self, data):
         """
         Build a pass list from a dataframe (and process row data)
         """
         pass_list = []
         for index, row in data.iterrows():
             pass_data = Pass(
-                round(row["x_begin"], 0), 
-                round(row["y_begin"], 0), 
-                round(row["x_end"], 0), 
-                round(row["y_end"], 0), 
+                self.to_bin(row["x_begin"], 2), 
+                self.to_bin(row["y_begin"], 2), 
+                self.to_bin(row["x_end"], 2), 
+                self.to_bin(row["y_end"], 2), 
                 row['game_id'], 
                 row['team_id'])
             pass_list.append(pass_data)
@@ -50,21 +53,48 @@ class SequencesFactory:
                 passe.x_begin - x_init,
                 passe.y_begin - y_init,
                 passe.x_end - x_init,
-                passe.y_end - y_init)
+                passe.y_end - y_init,
+                sequence[0].game_id,
+                sequence[0].team_id)
             new_sequence.append(new_passe)
         return new_sequence
 
-    def build_data(self, sequences, starting_window, ending_window):
-        null_pass = Pass(0, 0, 0, 0)
-        processed_sequences = []
-        header = list(numpy.array([[f"pass{i}_x_begin", f"pass{i}_y_begin", f"pass{i}_x_end", f"pass{i}_y_end"] for i in range(ending_window)]).flatten())
+    @staticmethod
+    def sequence_to_vec(sequence, save=False):
+        img = numpy.zeros((128,128,1), numpy.uint8)
+        colors = [int(155 + i * (255-155)/(len(sequence)-1)) for i in range(0, len(sequence))]
+        for i, passe in zip(range(len(sequence)), sequence):
+            img = cv2.line(img, (int(passe.x_begin),int(passe.y_begin)), (int(passe.x_end),int(passe.y_end)), (colors[i],0,0), 1)
+        if save:
+            img_folder = "model/pass2vec/resources"
+            now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            cv2.imwrite(f"{img_folder}/seq_{now}.png", img)
+        return img.flatten()
+
+    def build_data(self, sequences):
+        result = []
         for sequence in sequences:
-            if len(sequence) >= starting_window and len(sequence) <= ending_window:
-                padded_sequence = self.change_sequence_referentiel(sequence) + [null_pass for i in range(0, ending_window - len(sequence))]
-                processed_sequences.append(list(numpy.array([passe.geo for passe in padded_sequence]).flatten()))
-        print(processed_sequences[0])
-        print(header)
-        return pandas.DataFrame(processed_sequences, columns=header)
+            if len(sequence) > 1:
+                print(len(sequence))
+                row_image = self.sequence_to_vec(sequence, True)
+                result.append(row_image)
+        return result
+        
+
+    # def build_data(self, sequences, starting_window, ending_window):
+    #     null_pass = Pass(0, 0, 0, 0)
+    #     processed_sequences = []
+    #     header = list(numpy.array([[f"pass{i}_x_begin", f"pass{i}_y_begin", f"pass{i}_x_end", f"pass{i}_y_end"] for i in range(ending_window)]).flatten())
+    #     header += ["game_id", "team_id", "sequence_length"]
+    #     for sequence in sequences:
+    #         if len(sequence) >= starting_window and len(sequence) <= ending_window:
+    #             padded_sequence = self.change_sequence_referentiel(sequence) + [null_pass for i in range(0, ending_window  len(sequence))]
+    #             processed_sequences.append(
+    #                 list(numpy.array([passe.geo for passe in padded_sequence]).flatten()) +
+    #                 [padded_sequence[0].game_id] + 
+    #                 [padded_sequence[0].team_id] + 
+    #                 [len(sequence)])
+    #     return pandas.DataFrame(processed_sequences, columns=header)
 
 
     
--- model/pass2vec/train.py
+++ model/pass2vec/train.py
@@ -1,16 +1,22 @@
 import pandas
 import numpy
 from model.pass2vec.src.SequenceFactory import SequencesFactory
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+# from keras.models import Sequential
+# from keras.layers import Flatten, Dense
+# from sklearn.manifold import TSNE
 
 
 if __name__ == "__main__":
-    data = pandas.read_csv("model/pass2vec/resources/raw_passes.csv").head(100000)
+    data = pandas.read_csv("model/pass2vec/resources/raw_passes.csv").head(100000).dropna(axis=0)
     print(data.shape)
     sequences_factory = SequencesFactory()
     pass_list = sequences_factory.build_pass_list(data)
     sequences = sequences_factory.build_sequences(pass_list)
-    training_data = sequences_factory.build_data(sequences, 3, 6)
-    # training_data.to_csv("test.csv", index=False)
-    
+    result = sequences_factory.build_data(sequences)
+    print(len(result))
+    print(numpy.array(result).shape)
+    # training_data = sequences_factory.build_data(sequences, 2, 2)
+    # # training_data.to_csv("test.csv", index=False)
+    # data_embedded = TSNE(n_components=2).fit_transform(training_data.drop(["game_id", "team_id", "sequence_length"], axis=1).dropna(axis=0))
+    # result = pandas.concat((training_data, pandas.DataFrame(data_embedded, columns=["c1", "c2"])), axis=1)
+    # result.to_csv("result_tsne.csv", index=False)

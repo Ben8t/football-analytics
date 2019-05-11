406e4b011e43f6db79a1b1a0510e887fb9885e0d
Benoit Pimpaud <pimpaudben@gmail.com>
2019-05-11 22:37:43 +0200

    update lineup viz

--- src/sql_queries/lineup_evolution.sql
+++ src/sql_queries/lineup_evolution.sql
@@ -3,10 +3,11 @@ SELECT
 	player_name,
 	CASE WHEN
 		player_position LIKE '%Sub' THEN 'sub'
-	ELSE 'tit' END AS pos
+	ELSE 'tit' END AS position,
+	DENSE_RANK() OVER(ORDER BY "startDate") AS matchday
 FROM player_base
 INNER JOIN (SELECT game_id, "startDate" FROM metadata WHERE "startDate" > '2017-07-01' AND "startDate" < '2018-07-01') AS met
 ON met.game_id = player_base.game_id
-WHERE team_id = '32'
+WHERE team_id = '167'
 GROUP BY "startDate", player_name, player_position
 ORDER BY "startDate"
--- visualisation/lineup/process.r
+++ visualisation/lineup/process.r
@@ -1,34 +1,33 @@
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(hrbrthemes)
 library(showtext)
 
-font_add("ObjectSans","ObjectSans-Regular.otf")
-font_add("ObjectSansHeavy","ObjectSans-Heavy.otf")
-showtext_auto()
-data = read_csv("/data/data.csv")
+data <- read_csv("data/data.csv")
+
+data_order <- data %>% 
+	group_by(player_name, position) %>% 
+	summarise(n=n()) %>% 
+	filter(position=="tit") %>% 
+	select(player_name, n)
+
+graphic_data <- merge(data, data_order) %>% 
+	arrange(desc(n))
+
 graphic <- ggplot() + 
-			geom_point(data=data %>% filter(startDate > "2018-07-01") %>% mutate(match_day = match_day - 38), aes(x=match_day, y=player_name, color=lineup), size=2) +
+			geom_point(data=graphic_data, aes(x=matchday, y=reorder(player_name,desc(n)), color=position), size=2) +
 			scale_colour_manual(values=c("#ff8a80","#69f0ae")) + 
-			scale_x_continuous(breaks=c(1,5,10,15)) +
-			theme_minimal(base_family="ObjectSans", base_size=12) +
+			scale_x_continuous(breaks=c(1,5,10,15,20,25,30,35,38)) +
+			theme_minimal() +
 			theme(legend.background=element_blank()) +
-			theme(legend.key=element_blank()) +
+			theme(legend.key=element_b	lank()) +
 			theme(panel.grid=element_line(color="#cccccc", size=0.2)) +
 			theme(panel.grid.major=element_line(color="#cccccc", size=0.2)) +
 			theme(panel.grid.minor=element_line(color="#cccccc", size=0.15)) + 
 			theme(plot.title= element_text(family="ObjectSans", face="bold", size=15)) +
 			theme(legend.text= element_text(family="ObjectSans", size=12)) +
-			labs(title="Manchester United Squad Formation", 
+			labs(title="Manchester City Squad Formation", 
 				subtitle="Premier League 2018-2019 beginning of season",
 				x="Matchday",
-				y="") +
-			scale_y_discrete(limits=c("Romelu Lukaku", 
-				"Marcus Rashford", 
-				"Anthony Martial",
-				"Alexis Sánchez" ,
-				"Jesse Lingard",
-				"Juan Mata",
-				"Paul Pogba")
-			)
+				y="")
 		

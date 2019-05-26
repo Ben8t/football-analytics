d6c27a2836517d360adf89b976fdfb8155298a21
Benoit Pimpaud <pimpaudben@gmail.com>
2019-05-26 22:37:13 +0200

    add goal distribution and some update to pass2vec notebook

--- src/sql_queries/goal_distribution.sql
+++ src/sql_queries/goal_distribution.sql
@@ -1,3 +1,4 @@
+-- goals for
 WITH teams AS (
 	SELECT DISTINCT
 		home_team_id AS team_id,
@@ -14,4 +15,36 @@ LEFT JOIN events
 ON events.event_id = event_shots.event_id
 LEFT JOIN teams
 ON teams.team_id = event_shots.team_id
+WHERE is_goal=1
+
+-- goals against
+WITH teams AS (
+	SELECT DISTINCT
+		home_team_id AS team_id,
+		home_team_name AS team_name
+	FROM metadata
+)
+
+SELECT DISTINCT
+	event_shots.game_id,
+	event_shots.event_id,
+	events.minute,
+	teams.team_name,
+	metadata.home_team_name,
+	metadata.away_team_name,
+	CASE
+		WHEN teams.team_name=metadata.home_team_name THEN home_team_name
+		ELSE away_team_name
+	END AS goal_for,
+	CASE
+		WHEN teams.team_name=metadata.away_team_name THEN home_team_name
+		ELSE away_team_name
+	END AS goal_against
+FROM event_shots
+LEFT JOIN events
+ON events.event_id = event_shots.event_id
+LEFT JOIN teams
+ON teams.team_id = event_shots.team_id
+LEFT JOIN metadata
+ON metadata.game_id = event_shots.game_id
 WHERE is_goal=1
--- visualisation/goal_distribution/main.r
+++ visualisation/goal_distribution/main.r
@@ -1,1 +1,36 @@
-ggplot(data, aes(minute)) + geom_density(adjust=2) + theme_minimal() + facet_wrap(vars(home_team_name), ncol=3)
+library(tidyverse)
+library(viridis)
+library(hrbrthemes)
+
+data <- read_csv("data/data_min_team_against.csv")
+adjust <- 3/5
+teams_filter <- unique(data$goal_for)
+# teams_filter <- c("Liverpool", "Man City", "Arsenal", "Man Utd")
+
+# Goal for
+data_goal_for <- data %>% filter(goal_for %in% teams_filter)
+plot_goal_for <- ggplot(data_goal_for, aes(x=minute, color=goal_for)) + 
+    stat_density(geom="line", position="identity", adjust=adjust) +
+    scale_color_viridis_d(option="viridis") + 
+    theme_ipsum_rc() + 
+    labs(x="Minutes", y="Density",title="Goals distribution",subtitle="",caption="by @Ben8t",color="") +
+    theme(text=element_text(colour="white"),
+        plot.background=element_rect(fill="black"), 
+        axis.text=element_text(colour="white"), 
+        legend.text=element_text(colour="white"))
+
+ggsave("img/plot_goal_for.svg", plot_goal_for, width = 20, height = 10, units = "cm")
+
+# Goal against
+data_goal_against <- data %>% filter(goal_against %in% teams_filter)
+plot_goal_against <- ggplot(data_goal_against, aes(x=minute, color=goal_against)) + 
+    stat_density(geom="line", position="identity", adjust=adjust) +
+    scale_color_viridis_d(option="viridis") + 
+    theme_ipsum_rc() + 
+    labs(x="Minutes", y="Density",title="Conceded goals distribution",subtitle="",caption="by @Ben8t",color="") +
+    theme(text=element_text(colour="white"),
+        plot.background=element_rect(fill="black"), 
+        axis.text=element_text(colour="white"), 
+        legend.text=element_text(colour="white"))
+
+ggsave("img/plot_goal_against.svg", plot_goal_against, width = 20, height = 10, units = "cm")

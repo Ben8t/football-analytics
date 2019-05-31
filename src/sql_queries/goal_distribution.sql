9ba6b436a780782b08f2e1d347e4391af89ba1db
Benoit Pimpaud <pimpaudben@gmail.com>
2019-05-31 23:22:08 +0200

    update goal distribution

--- src/sql_queries/goal_distribution.sql
+++ src/sql_queries/goal_distribution.sql
@@ -1,23 +1,3 @@
--- goals for
-WITH teams AS (
-	SELECT DISTINCT
-		home_team_id AS team_id,
-		home_team_name AS team_name
-	FROM metadata
-)
-
-SELECT DISTINCT
-	event_shots.event_id,
-	minute,
-	teams.team_name
-FROM event_shots
-LEFT JOIN events
-ON events.event_id = event_shots.event_id
-LEFT JOIN teams
-ON teams.team_id = event_shots.team_id
-WHERE is_goal=1
-
--- goals against
 WITH teams AS (
 	SELECT DISTINCT
 		home_team_id AS team_id,
@@ -47,4 +27,5 @@ LEFT JOIN teams
 ON teams.team_id = event_shots.team_id
 LEFT JOIN metadata
 ON metadata.game_id = event_shots.game_id
-WHERE is_goal=1
+WHERE is_goal=1 AND
+metadata."startDate" > '2018-07-01'
--- visualisation/goal_distribution/main.r
+++ visualisation/goal_distribution/main.r
@@ -3,8 +3,8 @@ library(viridis)
 library(hrbrthemes)
 library(gghighlight)
 
-data <- read_csv("data/data_min_team_against.csv")
-adjust <- 4/5
+data <- read_csv("data/data_min_team.csv")
+adjust <- 1
 teams_filter <- unique(data$goal_for)
 # teams_filter <- c("Liverpool", "Man City", "Arsenal", "Man Utd")
 
@@ -13,7 +13,9 @@ data_goal_for <- data %>% filter(goal_for %in% teams_filter)
 plot_goal_for <- ggplot(data_goal_for, aes(x=minute, color=goal_for)) + 
     stat_density(geom="line", position="identity", adjust=adjust) +
     scale_color_viridis_d(option="viridis") + 
-    gghighlight(goal_for=="Arsenal" || goal_for=="Tottenham" || goal_for=="Man City" || goal_for=="Man Utd" || goal_for=="Liverpool" || goal_for=="Chelsea") + 
+    # gghighlight(goal_for=="Man City" || goal_for=="Liverpool") + 
+    gghighlight(goal_for=="Huddersfield" || goal_for=="Fulham" || goal_for=="Cardiff") + 
+    # gghighlight(goal_for=="Arsenal" || goal_for=="Tottenham" || goal_for=="Man City" || goal_for=="Man Utd" || goal_for=="Liverpool" || goal_for=="Chelsea") + 
     theme_ipsum_rc() + 
     labs(x="Minutes", y="Density",title="Goals distribution",subtitle="",caption="by @Ben8t",color="") +
     theme(text=element_text(colour="white"),
@@ -28,6 +30,9 @@ data_goal_against <- data %>% filter(goal_against %in% teams_filter)
 plot_goal_against <- ggplot(data_goal_against, aes(x=minute, color=goal_against)) + 
     stat_density(geom="line", position="identity", adjust=adjust) +
     scale_color_viridis_d(option="viridis") + 
+    # gghighlight(goal_against=="Man City" || goal_against=="Liverpool") + 
+    gghighlight(goal_against=="Huddersfield" || goal_against=="Fulham" || goal_against=="Cardiff") + 
+    # gghighlight(goal_against=="Arsenal" || goal_against=="Tottenham" || goal_against=="Man City" || goal_against=="Man Utd" || goal_against=="Liverpool" || goal_against=="Chelsea") + 
     theme_ipsum_rc() + 
     labs(x="Minutes", y="Density",title="Conceded goals distribution",subtitle="",caption="by @Ben8t",color="") +
     theme(text=element_text(colour="white"),

3109cdf058fe6bb87d57746968b29d396811c7f0
Benoit Pimpaud <pimpaudben@gmail.com>
2019-06-09 21:38:01 +0200

    add plots in goal distribution

--- visualisation/goal_distribution/main.r
+++ visualisation/goal_distribution/main.r
@@ -14,10 +14,10 @@ plot_goal_for <- ggplot(data_goal_for, aes(x=minute, color=goal_for)) +
     stat_density(geom="line", position="identity", adjust=adjust) +
     scale_color_viridis_d(option="viridis") + 
     # gghighlight(goal_for=="Man City" || goal_for=="Liverpool") + 
-    gghighlight(goal_for=="Huddersfield" || goal_for=="Fulham" || goal_for=="Cardiff") + 
+    # gghighlight(goal_for=="Huddersfield" || goal_for=="Fulham" || goal_for=="Cardiff") + 
     # gghighlight(goal_for=="Arsenal" || goal_for=="Tottenham" || goal_for=="Man City" || goal_for=="Man Utd" || goal_for=="Liverpool" || goal_for=="Chelsea") + 
     theme_ipsum_rc() + 
-    labs(x="Minutes", y="Density",title="Goals distribution",subtitle="",caption="by @Ben8t",color="") +
+    labs(x="Minutes", y="Density",title="Goals distribution",subtitle="Premier League 2018-2019",caption="by @Ben8t",color="") +
     theme(text=element_text(colour="white"),
         plot.background=element_rect(fill="black"), 
         axis.text=element_text(colour="white"), 
@@ -31,10 +31,10 @@ plot_goal_against <- ggplot(data_goal_against, aes(x=minute, color=goal_against)
     stat_density(geom="line", position="identity", adjust=adjust) +
     scale_color_viridis_d(option="viridis") + 
     # gghighlight(goal_against=="Man City" || goal_against=="Liverpool") + 
-    gghighlight(goal_against=="Huddersfield" || goal_against=="Fulham" || goal_against=="Cardiff") + 
+    # gghighlight(goal_against=="Huddersfield" || goal_against=="Fulham" || goal_against=="Cardiff") + 
     # gghighlight(goal_against=="Arsenal" || goal_against=="Tottenham" || goal_against=="Man City" || goal_against=="Man Utd" || goal_against=="Liverpool" || goal_against=="Chelsea") + 
     theme_ipsum_rc() + 
-    labs(x="Minutes", y="Density",title="Conceded goals distribution",subtitle="",caption="by @Ben8t",color="") +
+    labs(x="Minutes", y="Density",title="Conceded goals distribution",subtitle="Premier League 2018-2019",caption="by @Ben8t",color="") +
     theme(text=element_text(colour="white"),
         plot.background=element_rect(fill="black"), 
         axis.text=element_text(colour="white"), 

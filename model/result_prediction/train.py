733ab0362d67b4fd437a05bf1cc055acb68d2780
Benoit Pimpaud <pimpaudben@gmail.com>
2019-06-15 15:24:11 +0200

    [result_prediction] first training model with numeric features

--- model/result_prediction/train.py
+++ model/result_prediction/train.py
@@ -8,7 +8,6 @@ LABEL = "result"
 DROP_COLUMNS = ["game_id", "startDate"]
 
 def get_dataset(file_path):
-    # CSV columns in the input file.
     with open(file_path, "r") as f:
         names_row = f.readline()
     csv_columns = names_row.rstrip("\n").split(",")
@@ -24,13 +23,45 @@ def get_dataset(file_path):
         ignore_errors=True)
     return dataset
 
-def process_categorical_data(data, categories):
-    pass
+def numeric_features():
+    feature_columns = []
 
-def gather_categorical_data(dataframe):
-    pass
+    # numeric features
+    for team_context in ["home", "away"]:
+        for name in ["scored_goals", "conceded_goals", "expected_goals"]:
+            for i in range(0,5):
+                numeric_columns_name = f"{team_context}_{name}_{i}"
+                feature_columns.append(tf.feature_column.numeric_column(numeric_columns_name))
+    return feature_columns
+
+
 
 if __name__ == "__main__":
     train_set_file = "model/result_prediction/data/train_set.csv"   
     train_set = get_dataset(train_set_file)
-    import ipdb; ipdb.set_trace()
+    test_set_file = "model/result_prediction/data/test_set.csv"   
+    test_set = get_dataset(test_set_file)
+    
+    feature_columns = numeric_features()
+    
+    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+    model = tf.keras.Sequential([
+        feature_layer,
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(3, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'],
+                run_eagerly=True)
+
+    model.fit(train_set, epochs=100)
+
+    loss, accuracy = model.evaluate(test_set)
+    print("Accuracy", accuracy)

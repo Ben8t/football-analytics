c4d338fa918944ccf646bb8516c6113ed25a53aa
Benoit Pimpaud <pimpaudben@gmail.com>
2019-08-04 16:00:58 +0200

    [master] commit last code

--- src/sql_queries/lineup_evolution.sql
+++ src/sql_queries/lineup_evolution.sql
@@ -6,7 +6,7 @@ SELECT
 	ELSE 'tit' END AS position,
 	DENSE_RANK() OVER(ORDER BY "startDate") AS matchday
 FROM player_base
-INNER JOIN (SELECT game_id, "startDate" FROM metadata WHERE "startDate" > '2017-07-01' AND "startDate" < '2018-07-01') AS met
+INNER JOIN (SELECT game_id, "startDate" FROM metadata WHERE "startDate" > '2018-07-01' AND "startDate" < '2019-07-01') AS met
 ON met.game_id = player_base.game_id
 WHERE team_id = '167'
 GROUP BY "startDate", player_name, player_position
--- visualisation/lineup/process.r
+++ visualisation/lineup/process.r
@@ -3,7 +3,8 @@ library(ggplot2)
 library(hrbrthemes)
 library(showtext)
 
-data <- read_csv("data/data.csv")
+data <- read_csv("data/leicester_20182019.csv")
+image_path <- "img/leicester_20182019.svg"
 
 data_order <- data %>% 
 	group_by(player_name, position) %>% 
@@ -20,14 +21,19 @@ graphic <- ggplot() +
 			scale_x_continuous(breaks=c(1,5,10,15,20,25,30,35,38)) +
 			theme_minimal() +
 			theme(legend.background=element_blank()) +
-			theme(legend.key=element_b	lank()) +
+			theme(legend.key=element_blank()) +
 			theme(panel.grid=element_line(color="#cccccc", size=0.2)) +
 			theme(panel.grid.major=element_line(color="#cccccc", size=0.2)) +
 			theme(panel.grid.minor=element_line(color="#cccccc", size=0.15)) + 
 			theme(plot.title= element_text(family="ObjectSans", face="bold", size=15)) +
 			theme(legend.text= element_text(family="ObjectSans", size=12)) +
-			labs(title="Manchester City Squad Formation", 
-				subtitle="Premier League 2018-2019 beginning of season",
+			theme(text=element_text(colour="white"),
+				plot.background=element_rect(fill="black"), 
+				axis.text=element_text(colour="white"), 
+				legend.text=element_text(colour="white")) +
+			labs(title="Leicester Squad Formation", 
+				subtitle="Premier League 2018-2019 season",
 				x="Matchday",
 				y="")
 		
+ggsave(image_path, graphic, width = 20, height = 12, units = "cm")

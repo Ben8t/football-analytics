8fba1d80940ddb9e3095ff7abe8b564f1b7de609
Benoit Pimpaud <pimpaudben@gmail.com>
2019-08-19 22:04:39 +0200

    [expected_goal] add SQL query to build training data

---
+++ model/expected_goal/src/build_training_data.sql
@@ -0,0 +1,41 @@
+WITH shots AS (
+	SELECT 
+		game_id, 
+		event_id,
+		x_shot,
+		y_shot,
+		goal_distance,
+		big_chance
+	FROM event_shots
+)
+
+SELECT
+    game_id,
+    event_id,
+    team_id,
+    player_id,
+	minute,
+	second,
+	x_shot,
+	y_shot,
+	goal_distance,
+	big_chance,
+    previous_type_name,
+    previous_x,
+    previous_y
+FROM (
+    SELECT
+        events.*,
+        x_shot,
+        y_shot,
+        goal_distance,
+        big_chance,
+        LEAD(x) OVER(ORDER BY events.game_id, minute, second) AS previous_x,
+        LEAD(y) OVER(ORDER BY events.game_id, minute, second) AS previous_y,
+        LEAD(type_name) OVER(ORDER BY events.game_id, minute, second) AS previous_type_name
+    FROM events
+    LEFT JOIN shots
+    ON events.game_id = shots.game_id AND events.event_id = shots.event_id
+    ORDER BY events.game_id, minute, second
+) AS event_shots
+WHERE x_shot IS NOT NULL
